a08fe0816bd04819480a4156259a8d548829814c
Spockuto <venkythesupersaiyan@gmail.com>
2015-12-22 21:14:27 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Sha224 Tree
+===========
+
+*	A tree style hashing using sha224.
+*	Currently time reduced by 1 sec for 1GB file.
+
+![Block Based Hashing](image.png)
+
+
+Usage
+=====
+```sh
+$	python hash.py **file**
+```
+
+Things to do
+============
+
+*	[ ] Use multiprocessing 
+*	[ ] Increase Efficinecy
+
---
+++ hash.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import hashlib
+import sys
+
+def chunks( file_object , chunk_size=20971520):
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+if __name__ == '__main__':
+
+	big_file = open(sys.argv[1] ,'rb')
+	hashtree = ''
+	
+	for piece in chunks(big_file):
+		hashtree = hashtree + str(hashlib.sha224(piece).hexdigest())
+	
+	print hashlib.sha224(hashtree).hexdigest()
